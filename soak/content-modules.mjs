41b116ef4b7dc0b95df8a8cac637a6ad9651c16c
Veen <zwying0814@gmail.com>
2025-08-18 00:16:41 +0800

    feat: 添加静态资源文件和配置，包括图标、图片、字体、搜索功能、打赏组件等

---
+++ soak/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["../../content/posts/example_post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2F..%2Fcontent%2Fposts%2Fexample_post.mdx&astroContentModuleFlag=true")],
+["../../content/pages/example_page.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2F..%2Fcontent%2Fpages%2Fexample_page.mdx&astroContentModuleFlag=true")]]);
+		
